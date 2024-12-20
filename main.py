270464f2d606255f3fd6f20a403c360095257b36
LukastheHippo <lukas.the.hippo@gmail.com>
2024-12-20 09:25:32 -0600

    add sorted_list function to main.py

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = open_book(book_path)
     num_words = word_count(book_text)
-    print(f"There are {num_words} in the book.")
     counting_characters = character_count(book_text)
-    print(f"Count of each character in book: {counting_characters}")
+    sorted_list =character_sort(counting_characters)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"There are {num_words} in the book.")
+    print()
+    for entry in sorted_list:
+        print(f"The character '{entry["char"]}' was found {entry["count"]} times")
+    print(f"--- End report ---")
+
+    character_sort(counting_characters)
+
+def character_sort(char_count_dict):
+    sorted_list = []
+    for character in char_count_dict:
+        sorted_list.append({"char": f"{character}", "count": char_count_dict[character]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def sort_on(dict):
+    return dict["count"]
 
 def character_count(book_text):
     all_lowercase = book_text.lower() # convert string to lowercase
@@ -19,7 +37,6 @@ def character_count(book_text):
     
     return count_of_characters
         
-
 def word_count(book_text):
     count = 0
     words = book_text.split()
